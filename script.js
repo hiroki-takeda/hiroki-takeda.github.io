eb05af1f5564a341e12a6f80ff12e03fc6b0f5a3
hiroki-takeda <arrow7989@gmail.com>
2023-06-05 10:04:31 +0900

    for smartphone

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-window.onload = function() {
-  var navIcon = document.getElementById('nav-icon');
-  var navMenu = document.getElementById('nav-menu');
+window.addEventListener('DOMContentLoaded', (event) => {
+    const navIcon = document.getElementById('nav-icon');
+    const navMenu = document.getElementById('nav-menu');
 
-  navIcon.onclick = function() {
-    navMenu.classList.toggle('active');
-  }
-
-　window.onresize = function() {
-    if (window.innerWidth > 800) {
-        navMenu.style.display = '';
-        navMenu.classList.remove('active');
-    } else if (!navMenu.classList.contains('active')) {
-        navMenu.style.display = '';
+    // 画面の幅に基づいて、ナビゲーションメニューの表示を制御します。
+    function adjustMenu() {
+        if (window.innerWidth <= 480) {
+            navMenu.style.display = "flex";
+            navIcon.style.display = "none";
+        } else if (window.innerWidth <= 874) {
+            navMenu.style.display = "none";
+            navIcon.style.display = "flex";
+        } else {
+            navMenu.style.display = "flex";
+            navIcon.style.display = "none";
+        }
     }
-  }
-}
+
+    // 初回ページ読み込み時にメニュー表示を調整します。
+    adjustMenu();
+
+    // ハンバーガーアイコンをクリックしたときのイベントを設定します。
+    navIcon.addEventListener('click', function() {
+        if (navMenu.style.display === 'none') {
+            navMenu.style.display = 'flex';
+        } else {
+            navMenu.style.display = 'none';
+        }
+    });
+
+    // ウィンドウサイズの変更を検知して、メニュー表示を調整します。
+    window.addEventListener('resize', adjustMenu);
+});
