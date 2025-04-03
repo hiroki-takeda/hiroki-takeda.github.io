1135b6ebd25eb2fddac8e1f1c367ea2cdbfe31cd
hiroki-takeda <arrow7989@gmail.com>
2025-04-04 02:23:46 +0900

    Auto update

--- scripts/generate_publications_html.py
+++ scripts/generate_publications_html.py
@@ -138,7 +138,7 @@ def generate_section_html(title, entries):
 body_html = "<h1 id=\"publications\">Publications</h1>\n"
 body_html += generate_section_html("Books", categorized["book"])
 body_html += generate_section_html("Papers", categorized["short"])
-body_html = """
+body_html += """
 <h1 id="publications">Publications</h1>
 See also:
   <a href="https://arxiv.org/search/?query=%22Hiroki+Takeda%22&searchtype=author&abstracts=show&order=-announced_date_first&size=50">arXiv</a>, 
