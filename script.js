c1adece1c021726c7f1905a352de63c0f2c855bc
hiroki-takeda <arrow7989@gmail.com>
2025-04-04 10:52:33 +0900

    Auto update

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const navIcon = document.getElementById('nav-icon');
     const navMenu = document.getElementById('nav-menu');
-
-    // 画面の幅に基づいて、ナビゲーションメニューの表示を制御します。
+  
     function adjustMenu() {
-        if (window.innerWidth <= 874) {
-            navMenu.style.display = "none";
-            navIcon.style.display = "flex";
-        } else {
-            navMenu.style.display = "flex";
-            navIcon.style.display = "none";
-        }
+      if (window.innerWidth <= 874) {
+        navMenu.classList.remove('active');
+        navMenu.style.display = "none";  // 初期状態
+        navIcon.style.display = "flex";
+      } else {
+        navMenu.style.display = "flex";
+        navIcon.style.display = "none";
+      }
     }
-
-    // 初回ページ読み込み時にメニュー表示を調整します。
+  
     adjustMenu();
-
-    // ハンバーガーアイコンをクリックしたときのイベントを設定します。
-    navIcon.addEventListener('click', function() {
-        if (navMenu.style.display === 'none') {
-            navMenu.style.display = 'flex';
-        } else {
-            navMenu.style.display = 'none';
-        }
+  
+    navIcon.addEventListener('click', function () {
+      navMenu.classList.toggle('active');
     });
-
-    // ウィンドウサイズの変更を検知して、メニュー表示を調整します。
+  
     window.addEventListener('resize', adjustMenu);
-});
+  });
